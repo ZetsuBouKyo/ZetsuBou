ed71e7ca9ab0f8a45713ec9c03e052bd516bf29c
ZetsuBouKyo <116266262+ZetsuBouKyo@users.noreply.github.com>
2023-05-29 04:06:12 +0800

    fix: remove unused CSS class

--- front/src/components/Labels/index.vue
+++ front/src/components/Labels/index.vue
@@ -4,7 +4,7 @@
       class="h-8 transition-colors duration-200 ease-in mx-2 my-1 3xl:px-4 3xl:py-2 px-3 py-1 rounded-full max-w-full truncate"
       :class="
         searchable
-          ? 'cursor-pointer border border-gray-200 hover:border-indigo-500 hover:bg-indigo-500 hover:text-gray-100 hover:border-white'
+          ? 'cursor-pointer border border-gray-200 hover:border-indigo-500 hover:bg-indigo-500 hover:text-gray-100 '
           : 'cursor-default border border-gray-200'
       "
       v-for="(val, key) in labels"

cb7b76b9f910f71c3dc9f26fac72e1a2a2e96aee
ZetsuBouKyo <116266262+ZetsuBouKyo@users.noreply.github.com>
2023-05-28 12:50:19 +0800

    fix: change the location of `init_database`

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ from starlette.responses import JSONResponse
 
 from back.api import router as api
 from back.init.async_elasticsearch import init_indices
+from back.init.database import init_table
 from back.route import router as views
 from back.session.minio import init_minio
 from back.settings import setting

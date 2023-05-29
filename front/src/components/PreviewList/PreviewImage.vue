b3eb838624fefce5fc114fcb5feeac462c790d32
ZetsuBouKyo <116266262+ZetsuBouKyo@users.noreply.github.com>
2023-05-29 19:15:30 +0800

    feat: add loading icon and so on
    - Add loading svg before image is loaded.
    - Remove redundant code.

--- front/src/components/PreviewList/PreviewImage.vue
+++ front/src/components/PreviewList/PreviewImage.vue
@@ -1,47 +1,22 @@
-<script lang="ts">
-import { ref, reactive, PropType } from "vue";
-
-import { Item } from "./interface";
-import StarRating from "@/elements/Rating/StarRating.vue";
-
-export default {
-  components: { StarRating },
-  props: {
-    item: {
-      type: Object as PropType<Item>,
-      default: undefined,
-    },
-  },
-  setup(props) {
-    let image = ref(null);
-    const state = reactive({
-      width: undefined,
-      height: undefined,
-    });
-
-    function getIntrinsicSize(event) {
-      state.width = event.target.naturalWidth;
-      state.height = event.target.naturalHeight;
-    }
-
-    return { ...props, state, image, getIntrinsicSize };
-  },
-};
-</script>
-
 <template>
   <div>
     <div class="block relative overflow-hidden rounded-lg bg-gray-900">
       <div class="m-4 hover:opacity-50">
-        <a :href="item.linkUrl">
+        <a class="relative" :href="item.linkUrl">
           <img
             ref="image"
             alt="Not found!"
             loading="lazy"
             class="object-contain object-center w-full md:h-72 h-70v block animate-fade-in"
+            :class="state.loading ? 'invisible' : ''"
             :title="state.width ? state.width.toString() + ' x ' + state.height.toString() : ''"
             :src="item.imgUrl"
             @load="getIntrinsicSize"
+          />
+          <icon-eos-icons-loading
+            class="absolute m-auto top-0 bottom-0 left-0 right-0 animate-spin"
+            style="font-size: 2rem; color: white"
+            v-if="state.loading"
           />
         </a>
       </div>
@@ -61,9 +36,49 @@ export default {
           {{ item.title }}
         </h2>
         <div class="flex mt-2" v-if="item.timestamp">
-          <h2 class="text-white text-gray-500 ml-auto 3xl:text-xs">Last updated on {{ item.timestamp }}</h2>
+          <h2 class="text-white ml-auto 3xl:text-xs">Last updated on {{ item.timestamp }}</h2>
         </div>
       </div>
     </div>
   </div>
 </template>
+
+<script lang="ts">
+import { ref, reactive, PropType, watch } from "vue";
+
+import { Item } from "./interface";
+import StarRating from "@/elements/Rating/StarRating.vue";
+
+export default {
+  components: { StarRating },
+  props: {
+    item: {
+      type: Object as PropType<Item>,
+      default: undefined,
+    },
+  },
+  setup(props) {
+    let image = ref(null);
+    const state = reactive({
+      loading: true,
+      width: undefined,
+      height: undefined,
+    });
+
+    function getIntrinsicSize(event) {
+      state.width = event.target.naturalWidth;
+      state.height = event.target.naturalHeight;
+      state.loading = false;
+    }
+
+    watch(
+      () => props.item.imgUrl,
+      () => {
+        state.loading = true;
+      },
+    );
+
+    return { ...props, state, image, getIntrinsicSize };
+  },
+};
+</script>

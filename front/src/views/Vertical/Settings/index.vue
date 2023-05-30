24c5c0a69a89c4fdfc4d91b3679ae3e6e54016f3
ZetsuBouKyo <116266262+ZetsuBouKyo@users.noreply.github.com>
2023-05-30 16:20:24 +0800

    feat: update setting UI

--- front/src/views/Vertical/Settings/index.vue
+++ front/src/views/Vertical/Settings/index.vue
@@ -2,37 +2,50 @@
   <div class="flex flex-col w-2/3 mx-auto mt-4">
     <div class="flex"></div>
     <div class="flex flex-row">
-      <div class="flex flex-col w-1/6">
-        <setting-action :title="'Account'" :path="'/settings/account'">
-          <icon-ic-baseline-person class="views-setting-action-icon" style="font-size: 1.4rem" />
-        </setting-action>
-        <setting-action :title="'Appearance'" :path="'/settings/appearance'">
-          <icon-ic-outline-edit class="views-setting-action-icon" style="font-size: 1.4rem" />
-        </setting-action>
-        <setting-action :title="'Authentication'" :path="'/settings/authentication'">
-          <icon-ic-baseline-security class="views-setting-action-icon" style="font-size: 1.4rem" />
-        </setting-action>
-        <setting-action :title="'Storage'" :path="'/settings/storage-minio'">
-          <icon-mdi-storage class="views-setting-action-icon" style="font-size: 1.4rem" />
-        </setting-action>
-        <setting-action-dropdown :title="'Tag'" :options="tagOptions">
-          <template v-slot:icon>
-            <icon-ion-ios-pricetags class="views-setting-action-icon" style="font-size: 1.4rem" />
-          </template>
-        </setting-action-dropdown>
-        <setting-action-dropdown :title="'Elasticsearch'" :options="elasticsearchQueryOptions">
-          <template v-slot:icon>
-            <icon-ic-baseline-search class="views-setting-action-icon" style="font-size: 1.4rem" />
-          </template>
-        </setting-action-dropdown>
-        <setting-action-dropdown :title="'Quest'" :options="questOptions">
-          <template v-slot:icon>
-            <icon-icons8-todo-list class="views-setting-action-icon" style="font-size: 1.4rem" />
-          </template>
-        </setting-action-dropdown>
-        <setting-action :title="'Group'" :path="'/settings/group'">
-          <icon-ic-baseline-group class="views-setting-action-icon" style="font-size: 1.4rem" />
-        </setting-action>
+      <div class="flex flex-col w-1/6 divide-y divide-gray-500">
+        <div class="views-setting-actions">
+          <setting-action :title="'Account'" :path="'/settings/account'">
+            <icon-ic-baseline-person class="views-setting-action-icon" style="font-size: 1.4rem" />
+          </setting-action>
+          <setting-action :title="'Appearance'" :path="'/settings/appearance'">
+            <icon-ic-outline-edit class="views-setting-action-icon" style="font-size: 1.4rem" />
+          </setting-action>
+          <setting-action :title="'Authentication'" :path="'/settings/authentication'">
+            <icon-ic-baseline-security class="views-setting-action-icon" style="font-size: 1.4rem" />
+          </setting-action>
+        </div>
+        <div class="views-setting-actions">
+          <span class="views-setting-action-category">Search</span>
+          <setting-action-dropdown :title="'Tag'" :options="tagOptions">
+            <template v-slot:icon>
+              <icon-ion-ios-pricetags class="views-setting-action-icon" style="font-size: 1.4rem" />
+            </template>
+          </setting-action-dropdown>
+          <setting-action-dropdown :title="'Elasticsearch'" :options="elasticsearchQueryOptions">
+            <template v-slot:icon>
+              <icon-ic-baseline-search class="views-setting-action-icon" style="font-size: 1.4rem" />
+            </template>
+          </setting-action-dropdown>
+          <setting-action-dropdown :title="'Quest'" :options="questOptions">
+            <template v-slot:icon>
+              <icon-icons8-todo-list class="views-setting-action-icon" style="font-size: 1.4rem" />
+            </template>
+          </setting-action-dropdown>
+        </div>
+        <div class="views-setting-actions">
+          <span class="views-setting-action-category">Administration</span>
+          <setting-action :title="'Users'" :path="'/settings/users'">
+            <icon-ic-baseline-group class="views-setting-action-icon" style="font-size: 1.4rem" />
+          </setting-action>
+          <setting-action :title="'Group'" :path="'/settings/group'">
+            <icon-fa-group class="views-setting-action-icon" style="font-size: 1.4rem" />
+          </setting-action>
+          <setting-action-dropdown :title="'Storage'" :options="storageOptions">
+            <template v-slot:icon>
+              <icon-mdi-storage class="views-setting-action-icon" style="font-size: 1.4rem" />
+            </template>
+          </setting-action-dropdown>
+        </div>
       </div>
       <div class="flex w-full">
         <router-view />
@@ -65,7 +78,9 @@ export default {
       { title: "Quest", path: "/settings/quest" },
       { title: "Elastic Count Quest", path: "/settings/elasticsearch-count-quest" },
     ];
-    return { tagOptions, elasticsearchQueryOptions, questOptions };
+    const storageOptions = [{ title: "MinIO", path: "/settings/storage-minio" }];
+
+    return { tagOptions, elasticsearchQueryOptions, questOptions, storageOptions };
   },
 };
 </script>

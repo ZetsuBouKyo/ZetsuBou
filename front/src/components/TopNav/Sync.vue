f4ad6173b24fbbd7d76de606d3eda78c819702b9
ZetsuBouKyo <116266262+ZetsuBouKyo@users.noreply.github.com>
2023-06-05 16:54:18 +0800

    feat: update UI

--- front/src/components/TopNav/Sync.vue
+++ front/src/components/TopNav/Sync.vue
@@ -17,13 +17,22 @@
   <div class="relative mx-1 h-10">
     <dropdown class="text-white border-2 border-gray-700 hover:bg-gray-600 rounded-lg">
       <template v-slot:select>
-        <ripple-button class="focus:outline-none h-full">
-          <div class="inline-flex items-center" @click="synchronize">
-            <icon-ic-twotone-sync
-              class="m-2"
-              :class="state.isSync ? 'animate-spin' : ''"
-              style="font-size: 1.2rem; color: white"
-            />
+        <ripple-button class="focus:outline-none h-full rounded-l-lg">
+          <div class="flex flex-row my-auto" @click="synchronize">
+            <svg class="mx-2" width="24" height="24" viewBox="0 0 24 24" style="transform: rotate(-90deg)">
+              <circle cx="12" cy="12" r="10.8" fill="none" stroke="#e6e6e6" stroke-width="2.4" />
+              <circle
+                cx="12"
+                cy="12"
+                r="10.8"
+                fill="none"
+                stroke="#3b82f6"
+                stroke-width="2.4"
+                pathLength="100"
+                style="stroke-dasharray: 100; stroke-dashoffset: 100; stroke-linecap: round"
+                :style="'stroke-dashoffset:' + state.progress + ';'"
+              />
+            </svg>
             <span class="mr-4 my-auto">Sync</span>
           </div>
         </ripple-button>
@@ -34,6 +43,7 @@
           <select
             class="flex py-1 px-2 ml-auto mr-1 bg-gray-800 border-gray-700 w-36 self-center"
             v-model="state.action"
+            :disabled="state.isSync"
           >
             <option class="my-1 mx-2" value="all">All</option>
             <option class="my-1 mx-2" value="new" v-if="state.appMode === SettingAppMode.Standalone">New</option>
@@ -45,7 +55,7 @@
 </template>
 
 <script lang="ts">
-import { reactive, ref, watch, onBeforeMount } from "vue";
+import { reactive, ref, watch } from "vue";
 import { AxiosResponse } from "axios";
 
 import {
@@ -53,11 +63,17 @@ import {
   getTaskStandaloneSyncNewProgress,
   deleteTaskStandaloneSyncNewProgress,
 } from "@/api/v1/task/standalone";
+import {
+  postSyncStoragesMinio,
+  getTaskAirflowSyncStoragesProgress,
+  deleteTaskAirflowSyncStoragesProgress,
+} from "@/api/v1/task/airflow";
 
 import RippleButton from "@/elements/Button/RippleButton.vue";
 import Dropdown from "@/elements/Dropdown/Dropdown.vue";
 import ConfirmModal from "@/elements/Modal/ConfirmModal.vue";
 
+import { messageState } from "@/state/message";
 import { settingState, SettingAppMode } from "@/state/setting";
 
 enum SyncMode {
@@ -72,7 +88,9 @@ export default {
       appMode: undefined,
       isSync: false,
       action: undefined,
-      sync: undefined,
+      syncAll: undefined,
+      syncNew: undefined,
+      progress: 100,
     });
 
     function load() {
@@ -111,7 +129,6 @@ export default {
       if (state.isSync) {
         return;
       }
-      state.isSync = true;
       switch (state.action) {
         case SyncMode.New:
           confirmNew.value.open();
@@ -122,12 +139,12 @@ export default {
       }
     }
 
-    async function synchronizeHandler(handler: any) {
-      checkSyncState();
-      return handler()
+    async function synchronizeHandler(syncRequest: any, getProgressRequest: any, deleteProgressRequest: any) {
+      state.isSync = true;
+      checkSyncState(getProgressRequest, deleteProgressRequest);
+      return syncRequest()
         .then((response: AxiosResponse) => {
           if (response.status === 200) {
-            deleteProgress();
           }
         })
         .catch(() => {
@@ -137,43 +154,111 @@ export default {
     }
 
     function onConfirmSynchronizeNew() {
-      synchronizeHandler(getTaskStandaloneSyncNewGalleries);
+      synchronizeHandler(
+        getTaskStandaloneSyncNewGalleries,
+        getTaskStandaloneSyncNewProgress,
+        deleteTaskStandaloneSyncNewProgress,
+      );
     }
 
     function onConfirmSynchronizeAll() {
-      // synchronizeHandler(getTaskGallerySyncAll);
+      synchronizeHandler(
+        postSyncStoragesMinio,
+        getTaskAirflowSyncStoragesProgress,
+        deleteTaskAirflowSyncStoragesProgress,
+      );
     }
 
-    function deleteProgress() {
-      deleteTaskStandaloneSyncNewProgress().then((response: AxiosResponse) => {
-        if (response.status === 200) {
-          clearTimeout(state.sync);
-          state.isSync = false;
-        }
-      });
+    function deleteInterval(action: SyncMode) {
+      switch (action) {
+        case SyncMode.All:
+          clearInterval(state.syncAll);
+          state.syncAll = undefined;
+          break;
+        case SyncMode.New:
+          clearInterval(state.syncNew);
+          state.syncNew = undefined;
+          break;
+      }
     }
 
-    function getProgress() {
-      getTaskStandaloneSyncNewProgress().then((response) => {
-        if (response.status === 200) {
-          if (response.data.progress_id === null) {
-            clearTimeout(state.sync);
+    function deleteProgress(action: SyncMode, deleteRequest: any) {
+      deleteRequest()
+        .then((response: AxiosResponse) => {
+          if (response.status === 200) {
+            deleteInterval(action);
             state.isSync = false;
-          } else if (response.data.progress === 100) {
-            deleteProgress();
           }
-        }
-      });
+        })
+        .catch(() => {});
     }
 
-    function checkSyncState() {
-      state.isSync = true;
-      state.sync = setInterval(() => {
-        getProgress();
-      }, 1000);
+    function getProgress(action: SyncMode, getRequest: any, deleteRequest: any) {
+      getRequest()
+        .then((response: any) => {
+          if (response.status === 200) {
+            switch (response.data.progress) {
+              case null:
+                if (!state.isSync) {
+                  deleteInterval(action);
+                }
+                break;
+              case 100:
+                deleteProgress(action, deleteRequest);
+                state.isSync = false;
+                state.progress = 0;
+                messageState.push("Successfully synchronized storages");
+                break;
+              default:
+                state.isSync = true;
+                state.progress = 100 - response.data.progress;
+            }
+          }
+        })
+        .catch(() => {
+          deleteInterval(action);
+        });
     }
 
-    getProgress();
+    function checkSyncState(getProgressRequest: any, deleteProgressRequest: any) {
+      switch (state.action) {
+        case SyncMode.All:
+          if (state.syncAll === undefined) {
+            state.syncAll = setInterval(() => {
+              getProgress(SyncMode.All, getProgressRequest, deleteProgressRequest);
+            }, 2000);
+          }
+          break;
+        case SyncMode.New:
+          if (state.syncNew === undefined) {
+            state.syncNew = setInterval(() => {
+              getProgress(SyncMode.New, getProgressRequest, deleteProgressRequest);
+            }, 2000);
+          }
+          break;
+      }
+    }
+
+    function initSyncState() {
+      switch (state.action) {
+        case SyncMode.All:
+          checkSyncState(getTaskAirflowSyncStoragesProgress, deleteTaskAirflowSyncStoragesProgress);
+          break;
+        case SyncMode.New:
+          checkSyncState(getTaskStandaloneSyncNewProgress, deleteTaskStandaloneSyncNewProgress);
+          break;
+      }
+    }
+    initSyncState();
+    watch(
+      () => state.action,
+      () => {
+        state.progress = 100;
+        deleteInterval(SyncMode.All);
+        deleteInterval(SyncMode.New);
+        initSyncState();
+      },
+    );
 
     return {
       state,

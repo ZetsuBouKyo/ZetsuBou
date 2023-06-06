19e06ab5fdf3d6d90976655661866fccad9a5d32
ZetsuBouKyo <116266262+ZetsuBouKyo@users.noreply.github.com>
2023-06-06 12:04:20 +0800

    fix: remove Tailwind CSS warning
    The `purge`/`content` options have changed in Tailwind CSS v3.0 .

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -4,7 +4,7 @@ const plugin = require("tailwindcss/plugin");
 /** @type {import("@types/tailwindcss/tailwind-config").TailwindConfig } */
 module.exports = {
   mode: "jit",
-  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {
       animation: {

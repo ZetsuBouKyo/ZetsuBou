127b2f7202b4f515016244116074eab76069f01e
ZetsuBouKyo <116266262+ZetsuBouKyo@users.noreply.github.com>
2023-06-06 11:16:00 +0800

    fix: remove Tailwind warning
    As of Tailwind CSS v3.3, the `@tailwindcss/line-clamp` plugin is now
    included by default.

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -125,7 +125,6 @@ module.exports = {
   plugins: [
     require("@tailwindcss/forms"),
     require("@tailwindcss/typography"),
-    require("@tailwindcss/line-clamp"),
     require("@tailwindcss/aspect-ratio"),
     plugin(function ({ addUtilities }) {
       const scrollbar = {

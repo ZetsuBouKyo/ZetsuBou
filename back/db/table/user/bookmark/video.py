d5d7f7ee407bc254a670e2a0e9ae3df9b18f55a2
ZetsuBouKyo <116266262+ZetsuBouKyo@users.noreply.github.com>
2023-06-09 22:43:43 +0800

    feat: add video bookmark blueprint

---
+++ back/db/table/user/bookmark/video.py
@@ -0,0 +1,26 @@
+from back.utils.dt import iso2datetime
+from sqlalchemy import TIMESTAMP, VARCHAR, Column, ForeignKey, Integer
+from sqlalchemy.orm import validates
+from sqlalchemy.sql import functions as func
+
+from ...base import Base
+
+
+class UserBookmarkVideoBase(Base):
+    __tablename__ = "user_bookmark_video"
+    id = Column(Integer, primary_key=True, index=True)
+
+    user_id = Column(
+        Integer, ForeignKey("user.id", ondelete="CASCADE"), nullable=False, index=True
+    )
+
+    video_id = Column(VARCHAR(64), nullable=False)
+    time = Column(Integer, nullable=False)
+
+    modified = Column(TIMESTAMP, server_default=func.now())
+
+    @validates("modified")
+    def validate_modified(self, _, value):
+        if type(value) is str:
+            return iso2datetime(value)
+        return value

46db65b2e4ab465055d17da9ab4f595860a45e89
ZetsuBouKyo <116266262+ZetsuBouKyo@users.noreply.github.com>
2023-06-10 08:33:19 +0800

    refactor: switch from anchor to router-link

--- front/src/components/TopNav/Icon.vue
+++ front/src/components/TopNav/Icon.vue
@@ -1,6 +1,6 @@
 <template>
-  <a class="flex font-medium items-center text-white h-20" href="/">
+  <router-link class="flex font-medium items-center text-white h-20" to="/">
     <!-- <icon-mdi-menu style="font-size: 2em" /> -->
     <span class="lg:inline hidden ml-3 3xl:text-2xl text-xl">ZetsuBou</span>
-  </a>
+  </router-link>
 </template>

d71fd6faf34768286ac39c6fb6ef03215b4a79ee
ZetsuBouKyo <116266262+ZetsuBouKyo@users.noreply.github.com>
2023-06-15 15:39:45 +0800

    feat: add redirecting

--- back/init/route/views.py
+++ back/init/route/views.py
@@ -1,6 +1,12 @@
 from back.route.views import index
 from fastapi import APIRouter
+from fastapi.responses import RedirectResponse
 
 router = APIRouter()
 
 router.add_api_route("/initialization", index)
+
+
+@router.get("/")
+async def redirect_to_initialization():
+    return RedirectResponse("/initialization")

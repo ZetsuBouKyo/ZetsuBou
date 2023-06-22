03e810e6669384da6af431f89b5c8a9fe02127c2
ZetsuBouKyo <116266262+ZetsuBouKyo@users.noreply.github.com>
2023-06-23 04:07:09 +0800

    feat: add a command
    Generate the Typescript interfaces from `Setting`.

--- command/build/setting.py
+++ command/build/setting.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 import typer
 from back.settings import Setting, setting
 
@@ -33,3 +35,64 @@ def print_setting():
     Print setting in the form of JSON.
     """
     print(setting.json(indent=4))
+
+
+@app.command()
+def generate_interface(out: str = typer.Argument(..., help="Output path.")):
+    lines = []
+    schema = Setting.schema()
+    title = schema.get("title", None)
+    if title is None:
+        return
+
+    properties = schema.get("properties", None)
+    if properties is None:
+        return
+
+    definitions = schema.get("definitions", None)
+    for enum_name, enum_property in definitions.items():
+        if not enum_name.endswith("Enum"):
+            continue
+        enums = enum_property.get("enum", None)
+        if enums is None:
+            continue
+
+        lines.append(f"export enum {enum_name} {{")
+        for e in enums:
+            if type(e) is str:
+                lines.append(f'  {e.title()} = "{e}",')
+        lines.append("}")
+        lines.append("")
+
+    lines.append(f"export interface {title} {{")
+    for property_name, property in properties.items():
+        property_type = property.get("type", None)
+        if property_type is None:
+            all_of = property.get("allOf", None)
+            if all_of is None:
+                continue
+            ref = all_of[0].get("$ref", None)
+            if ref is None:
+                print(property_name)
+                continue
+            ref_key = Path(ref).name
+            if not ref_key.endswith("Enum"):
+                continue
+
+            property_enum = definitions.get(ref_key, None)
+            if property_enum is None:
+                continue
+            property_type = ref_key
+
+        if property_type == "integer":
+            property_type = "number"
+
+        lines.append(f"  {property_name}: {property_type};")
+
+    lines.append("}")
+    lines.append("")
+
+    out_path = Path(out)
+    with out_path.open(mode="w") as fp:
+        lines_str = "\n".join(lines)
+        fp.write(lines_str)

181bdcafbafe739a66386b1c954710d863a24f85
ZetsuBouKyo <116266262+ZetsuBouKyo@users.noreply.github.com>
2023-06-29 17:21:14 +0800

    feat: remove X in HTML input element
    Refs: https://github.com/tailwindlabs/tailwindcss/discussions/10190

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -166,5 +166,13 @@ module.exports = {
 
       addUtilities(ripple);
     }),
+    plugin(function ({ addBase }) {
+      addBase({
+        '[type="search"]::-webkit-search-decoration': { display: "none" },
+        '[type="search"]::-webkit-search-cancel-button': { display: "none" },
+        '[type="search"]::-webkit-search-results-button': { display: "none" },
+        '[type="search"]::-webkit-search-results-decoration': { display: "none" },
+      });
+    }),
   ],
 };

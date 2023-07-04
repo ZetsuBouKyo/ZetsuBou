7859e996f00deba4d382f491abfe75edd69c1d59
ZetsuBouKyo <116266262+ZetsuBouKyo@users.noreply.github.com>
2023-07-05 00:12:19 +0800

    refactor: transform to uppercase

--- back/model/task.py
+++ back/model/task.py
@@ -2,13 +2,13 @@ from enum import Enum
 
 from pydantic import BaseModel
 
-prefix = "zetsubou"
+PREFIX = "zetsubou"
 
 
 class ZetsuBouTaskProgressEnum(str, Enum):
-    SYNC_STORAGE: str = f"{prefix}.task.progress.sync-storage"
-    SYNC_STORAGES: str = f"{prefix}.task.progress.sync-storages"
-    SYNC_NEW_GALLERIES: str = f"{prefix}.task.progress.sync-new-galleries"
+    SYNC_STORAGE: str = f"{PREFIX}.task.progress.sync-storage"
+    SYNC_STORAGES: str = f"{PREFIX}.task.progress.sync-storages"
+    SYNC_NEW_GALLERIES: str = f"{PREFIX}.task.progress.sync-new-galleries"
 
 
 class ZetsuBouTask(BaseModel):

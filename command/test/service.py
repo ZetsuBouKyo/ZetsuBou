4e9ef5b4186f605b3c1014538d0b7aa56a617730
ZetsuBouKyo <116266262+ZetsuBouKyo@users.noreply.github.com>
2023-07-04 15:25:09 +0800

    feat: add a new command for testing host port

--- command/test/service.py
+++ command/test/service.py
@@ -2,6 +2,7 @@ import typer
 from back.init.check import ping as _ping
 from back.model.service import ServiceEnum
 from command.utils import sync
+from back.init.check import check_host_port
 
 app = typer.Typer(name="service")
 
@@ -13,3 +14,11 @@ async def ping(service: ServiceEnum = typer.Option(default=None, help="Service n
     Ping the services.
     """
     print(await _ping(service=service))
+
+
+@app.command()
+def port(port: int = typer.Argument(..., help="Service port.")):
+    """
+    Return `False` if host port is available.
+    """
+    print(check_host_port(port))

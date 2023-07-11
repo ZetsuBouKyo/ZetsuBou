ea3c7d3c0390ac222033605216a8404a8964bf29
ZetsuBouKyo <116266262+ZetsuBouKyo@users.noreply.github.com>
2023-07-11 21:48:12 +0800

    feat: add a new command

--- command/test/__init__.py
+++ command/test/__init__.py
@@ -1,6 +1,10 @@
+import typer
+
+from back.utils.keyword import parse_keywords
 from command.test.gallery import app as gallery
 from command.test.service import app as service
 from lib.typer import ZetsuBouTyper
+from lib.uvicorn.files import get_watched_files
 
 _help = """
 Build the testing cases.
@@ -9,3 +13,15 @@ app = ZetsuBouTyper(name="test", help=_help)
 
 app.add_typer(gallery)
 app.add_typer(service)
+
+
+@app.command(name="parse-keywords")
+def _parse_keywords(keywords: str = typer.Argument(..., help="Keywords.")):
+    parse_keywords(keywords=keywords)
+
+
+@app.command()
+def watch_files():
+    filenames = get_watched_files()
+    for filename in filenames:
+        print(filename)

88464e4efbf40bad506a4de81bc6f950f2c909fd
ZetsuBouKyo <116266262+ZetsuBouKyo@users.noreply.github.com>
2023-07-15 02:00:24 +0800

    feat: add a new command to generate favicon

--- command/build/statics.py
+++ command/build/statics.py
@@ -1,6 +1,7 @@
 import typer
 from back.init.statics import get_static_file
-
+from PIL import Image
+from pathlib import Path
 from lib.typer import ZetsuBouTyper
 
 _help = """
@@ -15,3 +16,21 @@ async def download(
     statics_home: str = typer.Argument(..., help="Static home."),
 ):
     await get_static_file(url, statics_home)
+
+
+@app.command()
+def favicon(
+    source: str = typer.Argument(..., help="Source image."),
+    out: str = typer.Argument(..., help="Output folder."),
+):
+    _source = Path(source)
+    if not _source.exists() or not _source.is_file():
+        return
+    _out = Path(out)
+    if not _out.is_dir():
+        return
+
+    filepath = _out / "favicon.ico"
+
+    img = Image.open(source)
+    img.save(filepath)

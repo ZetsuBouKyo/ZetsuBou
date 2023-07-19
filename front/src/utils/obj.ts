bacaf0856f0d0f7390ea1c4c8f61601fe812eaf1
ZetsuBouKyo <116266262+ZetsuBouKyo@users.noreply.github.com>
2023-07-19 23:14:06 +0800

    feat: add functions for getting and setting object

--- front/src/utils/obj.ts
+++ front/src/utils/obj.ts
@@ -7,3 +7,15 @@ export function isEmpty(obj: object) {
 
   return JSON.stringify(obj) === JSON.stringify({});
 }
+
+export function getValue(obj: any, key: string | Array<string>, separator: string = ".") {
+  const keys = Array.isArray(key) ? key : key.split(separator);
+  return keys.reduce((prev, curr) => prev?.[curr], obj);
+}
+
+export function setValue(obj: any, key: string | Array<string>, value: any, separator: string = ".") {
+  const keys = Array.isArray(key) ? key : key.split(separator);
+  const lastKey = keys.pop();
+  const lastObj = keys.reduce((prev, curr) => prev?.[curr], obj);
+  lastObj[lastKey] = value;
+}

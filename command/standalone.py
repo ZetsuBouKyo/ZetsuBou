028b71ca2bdbbf0be0f651bf7b6439fe7033e05a
ZetsuBouKyo <116266262+ZetsuBouKyo@users.noreply.github.com>
2023-07-26 21:36:35 +0800

    feat: add a timer

--- command/standalone.py
+++ command/standalone.py
@@ -1,3 +1,5 @@
+import time
+
 import typer
 from rich import print_json
 
@@ -28,5 +30,12 @@ async def _get_storage_stat(
     protocol: SourceProtocolEnum = typer.Argument(..., help="Storage protocol."),
     storage_id: int = typer.Argument(..., help="Storage ID."),
 ):
+    """
+    Get storage stat via Python `pathlib`.
+    """
+    t0 = time.time()
     stat = await get_storage_stat(protocol, storage_id)
     print_json(data=stat.dict())
+    t1 = time.time()
+    diff = t1 - t0
+    print(f"time: {diff} (s)")

c44f8e37aa8c5532f8bd64f8c1af7e72ce49acc9
ZetsuBouKyo <116266262+ZetsuBouKyo@users.noreply.github.com>
2023-09-14 22:27:12 +0800

    feat: add `isPositiveInteger`

--- front/src/utils/number.ts
+++ front/src/utils/number.ts
@@ -5,3 +5,12 @@ export function pad(num: number, size: number): string {
   }
   return numS;
 }
+
+export function isPositiveInteger(num: number): boolean {
+  if (num > 2147483647 && parseInt(num.toString()) == num && num > 0) {
+    return true;
+  } else if ((num & 2147483647) == num) {
+    return true;
+  }
+  return false;
+}

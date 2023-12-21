ed639443f90faeae5e143ad90e760657d8cedda2
ZetsuBouKyo <116266262+ZetsuBouKyo@users.noreply.github.com>
2023-12-21 20:36:50 +0800

    test: add an exception

---
+++ tests/general/exceptions.py
@@ -0,0 +1,3 @@
+class NotFoundException(Exception):
+    def __init__(self, message: str):
+        super().__init__(message)
--- tests/general/session/async_elasticsearch.py
+++ tests/general/session/async_elasticsearch.py
@@ -3,13 +3,14 @@ from logging import Logger
 import pytest
 
 from back.init.check import ping_elasticsearch
+from tests.general.exceptions import NotFoundException
 
 
 class ElasticsearchSession:
     async def __aenter__(self):  # pragma: no cover
         ping = await ping_elasticsearch()
         if not ping:
-            pytest.exit("Skip because we can't connect to Elasticsearch.", returncode=0)
+            raise NotFoundException("Elasticsearch not found.")
 
     async def __aexit__(self, exc_type, exc, tb):  # pragma: no cover
         ...

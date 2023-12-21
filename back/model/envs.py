68524fe8fcb848befc5db5a17fee2c8c72a03ddd
ZetsuBouKyo <116266262+ZetsuBouKyo@users.noreply.github.com>
2023-12-21 20:34:35 +0800

    feat: update `ZetsuBouEnvEnum`

--- back/model/envs.py
+++ back/model/envs.py
@@ -2,4 +2,5 @@ from enum import Enum
 
 
 class ZetsuBouEnvEnum(str, Enum):
+    ZETSUBOU_ELASTIC_URLS: str = "ZETSUBOU_ELASTIC_URLS"
     ZETSUBOU_SETTING_PATH: str = "ZETSUBOU_SETTING_PATH"  # See `dags/cmd_tasks.py`

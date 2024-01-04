d078a20f3f78a9bbe247ce0a62e61222957096ff
ZetsuBouKyo <116266262+ZetsuBouKyo@users.noreply.github.com>
2024-01-04 16:32:06 +0800

    test: modify test report

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,11 +3,14 @@ import sys
 from pathlib import Path
 
 import pytest
+from _pytest.reports import TestReport
+from rich.console import Console
 
 from back.init.logger import stream_handler
 from back.logging.utils import get_all_loggers
 from lib.httpx import ZetsuBouAsyncClient
 from tests.general.logger import logger as _logger
+from tests.general.summary import divider
 
 sys.path.append(str(Path.cwd()))
 
@@ -19,11 +22,28 @@ def client() -> ZetsuBouAsyncClient:
     return ZetsuBouAsyncClient(app=app, base_url="http://test")
 
 
-def pytest_sessionstart(session):
-    print(type(session))
+def pytest_sessionstart():
     loggers = get_all_loggers()
     for logger in loggers:
         logger.setLevel(logging.WARNING)
         logger.handlers = []
     _logger.addHandler(stream_handler)
     _logger.setLevel(logging.DEBUG)
+
+
+def pytest_report_teststatus(report: TestReport):
+    console = Console()
+    if report.when == "call":
+        if report.failed:
+            divider(title="failed", characters="=", text_style="red")
+            return (report.outcome, "", "failed")
+        elif report.passed:
+            divider(title="passed", characters="=", text_style="green")
+            return (report.outcome, "", "passed")
+        elif report.skipped:
+            divider(title="skipped", characters="=", text_style="white")
+            return (report.outcome, "", "skipped")
+    elif report.when == "setup":
+        func_name = report.location[-1]
+        console.print("\n")
+        console.print(f"[blue]function [green]{func_name}")
--- tests/general/summary.py
+++ tests/general/summary.py
@@ -1,7 +1,25 @@
+from typing import Union
+
 from rich.console import Console
 from rich.rule import Rule
+from rich.style import Style
+from rich.text import Text
 
 
-def divider():
+def divider(
+    title: Union[str, Text] = "",
+    characters: str = "─",
+    text_style: Union[str, Style] = "",
+    rule_style: Union[str, Style] = "rule.line",
+):
+    if type(title) is str:
+        title = Text(text=title, style=text_style)
+
     console = Console()
-    console.print(Rule(title=""))
+    console.print(
+        Rule(
+            title=title,
+            characters=characters,
+            style=rule_style,
+        )
+    )

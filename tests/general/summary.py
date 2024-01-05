ff87949a8b9a3806ad051266f0f1868bd8d6d427
ZetsuBouKyo <116266262+ZetsuBouKyo@users.noreply.github.com>
2024-01-05 23:30:21 +0800

    test: add new functions

--- tests/general/summary.py
+++ tests/general/summary.py
@@ -1,9 +1,12 @@
 from typing import Union
 
+from httpx import Response
 from rich.console import Console
 from rich.rule import Rule
 from rich.style import Style
 from rich.text import Text
+
+from tests.general.logger import logger
 
 
 def print_divider(
@@ -23,3 +26,16 @@ def print_divider(
             style=rule_style,
         )
     )
+
+
+def print_api_request(url: str, method: str, data: dict = None):
+    method = method.upper()
+    logger.info(f"Request URL: {url}")
+    logger.info(f"Request method: {method}")
+    if data is not None:
+        logger.info(f"Request data: {data}")
+
+
+def print_api_response(response: Response):
+    logger.info(f"Response status code: {response.status_code}")
+    logger.info(f"Response text: {response.text}")

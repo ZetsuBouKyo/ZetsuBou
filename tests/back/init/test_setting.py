3b401019e0a5f6fcbc7655aaa95a7aee819aadc0
ZetsuBouKyo <116266262+ZetsuBouKyo@users.noreply.github.com>
2024-01-07 22:43:19 +0800

    test: update

---
+++ tests/back/init/test_setting.py
@@ -0,0 +1,16 @@
+from back.init.setting import init_example_settings, init_settings_with_examples
+from back.settings import Setting
+from tests.general.logger import logger
+
+
+def test_init_settings_with_examples():
+    s = Setting()
+    s = init_settings_with_examples(s)
+    logger.info(f"\n{s.model_dump_json(indent=4)}")
+    assert type(s) is Setting
+
+
+def test_init_example_settings():
+    s = init_example_settings()
+    logger.info(s.model_dump_json(indent=4))
+    assert type(s) is Setting

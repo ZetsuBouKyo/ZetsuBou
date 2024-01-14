133545217a0354b2d2c8fc60aa58001ec05453a1
ZetsuBouKyo <116266262+ZetsuBouKyo@users.noreply.github.com>
2024-01-14 15:15:14 +0800

    test: add a new function

--- tests/general/api.py
+++ tests/general/api.py
@@ -6,3 +6,8 @@ def get_admin_headers() -> dict:
     token = create_access_token("1", groups=[BuiltInGroupEnum.admin.value])
     bearer = f"Bearer {token}"
     return {"Authorization": bearer}
+
+
+def get_admin_cookies():
+    token = create_access_token("1", groups=[BuiltInGroupEnum.admin.value])
+    return {"token": token}

e7fef8a4300e993829b82b97caf6593811afa661
ZetsuBouKyo <116266262+ZetsuBouKyo@users.noreply.github.com>
2024-03-07 19:34:21 +0800

    feat: update

--- back/utils/url.py
+++ back/utils/url.py
@@ -3,7 +3,19 @@ from urllib.parse import urlparse
 
 def get_host(url: str, endswith_slash: bool = False) -> str:
     parsed_url = urlparse(url)
-    host = f"{parsed_url.scheme}://{parsed_url.hostname}:{parsed_url.port}"
+    scheme = parsed_url.scheme
+    hostname = parsed_url.hostname
+    port = parsed_url.port
+    if not scheme:
+        raise ValueError("scheme not found")
+    if not hostname:
+        raise ValueError("hostname not found")
+
+    host = f"{scheme}://{hostname}"
+
+    if port is not None:
+        host += f":{port}"
+
     if endswith_slash:
         host += "/"
     return host

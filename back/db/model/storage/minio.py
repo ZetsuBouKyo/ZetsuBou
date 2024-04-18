aded861bf7b61184d5677df45d520afb0c743501
ZetsuBouKyo <116266262+ZetsuBouKyo@users.noreply.github.com>
2024-04-18 17:35:04 +0800

    feat: add new properties

--- back/db/model/storage/minio.py
+++ back/db/model/storage/minio.py
@@ -2,6 +2,7 @@ from typing import Optional
 
 from pydantic import BaseModel, ConfigDict
 
+from back.model.base import SourceBaseModel, SourceProtocolEnum
 from back.model.storage import StorageCategoryEnum
 from back.utils.model import HttpUrlStr
 
@@ -21,6 +22,17 @@ class StorageMinioCreate(BaseModel):
 
 class StorageMinioCreated(StorageMinioCreate):
     id: int
+
+    @property
+    def path(cls) -> str:
+        if hasattr(cls, "_path"):
+            return cls._path
+        cls._path = f"{SourceProtocolEnum.MINIO.value}-{cls.id}://{cls.bucket_name}/{cls.prefix}"
+        return cls._path
+
+    @property
+    def source(cls) -> SourceBaseModel:
+        return SourceBaseModel(path=cls.path)
 
 
 StorageMinio = StorageMinioUpdate = StorageMinioCreated
--- tests/back/db/model/storage/test_minio.py
+++ tests/back/db/model/storage/test_minio.py
@@ -1,7 +1,9 @@
 from back.db.model import StorageMinio
+from back.model.base import SourceProtocolEnum
 from back.model.storage import StorageCategoryEnum
 from lib.faker import ZetsuBouFaker
 from tests.general.logger import logger
+from tests.general.summary import print_divider
 
 
 def test_right_data():
@@ -20,12 +22,23 @@ def test_right_data():
         },
         {
             "id": 1,
-            "category": StorageCategoryEnum.gallery,
+            "category": StorageCategoryEnum.gallery.value,
+            "name": faker.name(),
+            "endpoint": faker.url(),
+            "bucket_name": faker.random_string(10),
+            "prefix": f"{faker.name()}/",
+            "depth": 2,
+            "access_key": faker.random_string(10),
+            "secret_key": faker.random_string(16),
+        },
+        {
+            "id": 1,
+            "category": StorageCategoryEnum.video,
             "name": faker.name(),
             "endpoint": faker.url(),
             "bucket_name": faker.random_string(10),
             "prefix": f"{faker.name()}/{faker.name()}/",
-            "depth": 1,
+            "depth": -1,
             "access_key": faker.random_string(10),
             "secret_key": faker.random_string(16),
         },
@@ -34,4 +47,11 @@ def test_right_data():
     for data in right_data:
         for key, value in data.items():
             logger.info(f"{key}: {value}")
-        StorageMinio(**data)
+        storage = StorageMinio(**data)
+        id = data.get("id")
+        bucket_name = data.get("bucket_name")
+        prefix = data.get("prefix")
+        path = f"{SourceProtocolEnum.MINIO.value}-{id}://{bucket_name}/{prefix}"
+        assert storage.path == path
+        assert storage.source.path == path
+        print_divider()

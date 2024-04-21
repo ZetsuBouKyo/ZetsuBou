3bda9ff79210dfe8b3fc024fb5aa93f4975a3e12
ZetsuBouKyo <116266262+ZetsuBouKyo@users.noreply.github.com>
2024-04-21 21:55:44 +0800

    test: add a new session

--- tests/general/session/__init__.py
+++ tests/general/session/__init__.py
@@ -1,11 +1,15 @@
 from tests.general.session.async_elasticsearch import ElasticsearchSession
-from tests.general.session.async_integration import BaseIntegrationSession
+from tests.general.session.async_integration import (
+    BaseIntegrationSession,
+    SimpleGalleryIntegrationSession,
+)
 from tests.general.session.db.base import DatabaseSession
 from tests.general.session.db.user import UserSession
 
 __all__ = [
-    "ElasticsearchSession",
-    "DatabaseSession",
-    "UserSession",
     "BaseIntegrationSession",
+    "DatabaseSession",
+    "ElasticsearchSession",
+    "SimpleGalleryIntegrationSession",
+    "UserSession",
 ]

8cfc9579eb1f5c7bbb63944ac45358e60a11125e
ZetsuBouKyo <116266262+ZetsuBouKyo@users.noreply.github.com>
2024-05-07 17:29:36 +0800

    feat: add bidict

--- back/utils/map.py
+++ back/utils/map.py
@@ -1,7 +1,15 @@
-from typing import Any, List, Optional
+from collections import defaultdict
+from typing import Any, Generic, List, Optional, TypeVar, Union
+
+_KT = TypeVar("_KT")  # Key type.
+_VT = TypeVar("_VT")  # Value type.
 
 
 class OneToOneMap:
+    """
+    Restructure a list of dict to a one-to-one dict.
+    """
+
     def __init__(self, data: List[dict], key_name: str, value_name: str):
         self.raw_data = data
         self._key_to_value = {}
@@ -21,3 +29,36 @@ class OneToOneMap:
 
     def get_key(self, value: Any) -> Optional[Any]:
         return self._value_to_key.get(value, None)
+
+
+class BiDict(dict, Generic[_KT, _VT]):
+    def __setitem__(self, key: Union[_KT, _VT], value: Union[_KT, _VT]):
+        if key == value:
+            raise ValueError(f"key: {key} and value: {value} should not be the same.")
+
+        if key is None or value is None:
+            raise ValueError(f"key or value should not be None.")
+
+        if key in self and value in self:
+            return
+
+        if key in self:
+            del self[key]
+        if value in self:
+            del self[value]
+        dict.__setitem__(self, key, value)
+        dict.__setitem__(self, value, key)
+
+    def __getitem__(self, key: Union[_KT, _VT]) -> Union[_KT, _VT, None]:
+        try:
+            value = dict.__getitem__(self, key)
+            return value
+        except KeyError:
+            return None
+
+    def __delitem__(self, key: Union[_KT, _VT]):
+        dict.__delitem__(self, self[key])
+        dict.__delitem__(self, key)
+
+    def __len__(self) -> int:
+        return dict.__len__(self) // 2
--- tests/back/utils/test_map.py
+++ tests/back/utils/test_map.py
@@ -1,6 +1,6 @@
 import pytest
 
-from back.utils.map import OneToOneMap
+from back.utils.map import BiDict, OneToOneMap
 
 
 def test_one_to_one_map():
@@ -28,3 +28,60 @@ def test_one_to_one_map_exceptions():
     ]
     with pytest.raises(ValueError):
         OneToOneMap(data_2, "id", "name")
+
+
+def test_bidict_default():
+    b = BiDict()
+    assert b.get(1) is None
+    assert b.get(2) is None
+    assert b[3] is None
+    assert len(b) == 0
+
+
+def test_bidict():
+    b = BiDict()
+    b[1] = "red"
+    b["green"] = 2
+    b[3] = "blue"
+
+    assert len(b) == 3
+    assert b[1] == "red"
+    assert b["red"] == 1
+    assert b[2] == "green"
+    assert b["green"] == 2
+    assert b[3] == "blue"
+    assert b["blue"] == 3
+
+    b[1] = "white"
+    assert len(b) == 3
+    assert b[1] == "white"
+    assert b["white"] == 1
+
+    b[1] = "white"
+    assert len(b) == 3
+    assert b[1] == "white"
+    assert b["white"] == 1
+
+    b[4] = "white"
+    assert len(b) == 3
+    assert b[4] == "white"
+    assert b["white"] == 4
+    assert b[1] is None
+
+
+def test_bidict_exceptions():
+    with pytest.raises(ValueError):
+        b = BiDict()
+        b[1] = 1
+
+    with pytest.raises(ValueError):
+        b = BiDict()
+        b[1] = None
+
+    with pytest.raises(ValueError):
+        b = BiDict()
+        b[None] = 1
+
+
+def test_bidict_type_hint():
+    BiDict[int, str]

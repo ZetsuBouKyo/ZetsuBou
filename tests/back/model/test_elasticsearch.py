18bf5ebe96d74fd0b0c3dce69fc7c400949e8ba4
ZetsuBouKyo <116266262+ZetsuBouKyo@users.noreply.github.com>
2024-05-12 00:12:35 +0800

    test: elasticsearch model

--- tests/back/model/test_elasticsearch.py
+++ tests/back/model/test_elasticsearch.py
@@ -3,3 +3,13 @@ from back.model.elasticsearch import ElasticsearchAnalyzerEnum
 
 def test_elasticsearch_analyzer_enum():
     assert ElasticsearchAnalyzerEnum.DEFAULT == ElasticsearchAnalyzerEnum.DEFAULT.value
+
+
+def test_elasticsearch_analyzer_enum():
+    d = {ElasticsearchAnalyzerEnum.DEFAULT: 1}
+    assert d.get(ElasticsearchAnalyzerEnum.DEFAULT.value, None) == 1
+    assert d.get(ElasticsearchAnalyzerEnum.DEFAULT, None) == 1
+
+    d = {ElasticsearchAnalyzerEnum.DEFAULT.value: 1}
+    assert d.get(ElasticsearchAnalyzerEnum.DEFAULT.value, None) == 1
+    assert d.get(ElasticsearchAnalyzerEnum.DEFAULT, None) == 1

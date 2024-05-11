e4e8692c92eb93ec16910057251cadac1d51ab57
ZetsuBouKyo <116266262+ZetsuBouKyo@users.noreply.github.com>
2024-05-12 00:14:03 +0800

    feat: add a command to generate test data

--- command/test/__init__.py
+++ command/test/__init__.py
@@ -1,7 +1,14 @@
 import typer
 
 from back.model.image import ImageFormatEnum
+from back.utils.gen.gallery import (
+    generate_nested_20200_galleries,
+    generate_nested_galleries,
+    generate_simple_galleries,
+)
+from back.utils.gen.tag import generate_tag_attributes, generate_tags
 from back.utils.keyword import KeywordParser
+from command.logging import logger
 from command.test.gallery import app as gallery
 from command.test.route import app as route
 from command.test.service import app as service
@@ -18,6 +25,18 @@ app.add_typer(gallery)
 app.add_typer(route)
 app.add_typer(service)
 app.add_typer(video)
+
+
+@app.command()
+async def integration():
+    await generate_tag_attributes()
+    await generate_tags()
+
+    await generate_simple_galleries()
+    await generate_nested_galleries()
+    await generate_nested_20200_galleries()
+
+    logger.info("finished")
 
 
 @app.command(name="parse-keywords")

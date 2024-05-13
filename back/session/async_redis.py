60f74361f723544bf5f5e98de647be8735d1a27b
ZetsuBouKyo <116266262+ZetsuBouKyo@users.noreply.github.com>
2024-05-13 22:40:28 +0800

    fix: code is unreachable

--- back/session/async_redis.py
+++ back/session/async_redis.py
@@ -1,10 +1,14 @@
+from typing import Optional
+
 from back.settings import setting
 from lib.redis import ZetsuBouAsyncRedis
 
 REDIS_URL = setting.redis_url
 
 
-def get_async_redis_session(url: str = REDIS_URL, **kwargs) -> ZetsuBouAsyncRedis:
+def get_async_redis_session(
+    url: Optional[str] = REDIS_URL, **kwargs
+) -> ZetsuBouAsyncRedis:
     if url is not None:
         return ZetsuBouAsyncRedis.from_url(url, **kwargs)
     return None

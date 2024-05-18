3c3943d40c226b58b9696b4ddeab7c3b8f37f420
ZetsuBouKyo <116266262+ZetsuBouKyo@users.noreply.github.com>
2024-05-18 23:26:43 +0800

    feat: add a switch element

---
+++ front/src/elements/Switch/index.vue
@@ -0,0 +1,33 @@
+<script>
+export default {
+  name: "Switch",
+  props: {
+    modelValue: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  computed: {
+    isOn() {
+      return this.modelValue;
+    },
+  },
+  methods: {
+    toggleSwitch() {
+      this.$emit("update:modelValue", !this.isOn);
+    },
+  },
+};
+</script>
+
+<template>
+  <div
+    class="relative inline-flex items-center cursor-pointer rounded-full h-8"
+    :class="{ 'bg-indigo-500': isOn, 'bg-gray-300': !isOn }"
+    @click="toggleSwitch">
+    <span class="inline-block w-14 h-8 rounded-full transition-colors duration-300 ease-in-out"></span>
+    <span
+      class="absolute left-0 inline-block w-6 h-6 bg-white rounded-full shadow transform transition-transform duration-300 ease-in-out"
+      :class="{ 'translate-x-7': isOn, 'translate-x-1': !isOn }"></span>
+  </div>
+</template>
